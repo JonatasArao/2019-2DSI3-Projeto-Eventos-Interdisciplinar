8d481af845b22556dbb584f817d7b3b118db9342
AleqCorrea <franciscogabrielkawai@gmail.com>
2019-10-30 20:07:23 -0300

    add tb_usuario

--- script.sql
+++ script.sql
@@ -2,11 +2,15 @@ CREATE DATABASE db_evento;
 
 USE db_evento;
 
-CREATE TABLE tb_administrador(
-	cd_administrador INT PRIMARY KEY AUTO_INCREMENT,
-	nm_administrador VARCHAR(45),
+CREATE TABLE tb_usuario(
+	cd_usuario INT PRIMARY KEY AUTO_INCREMENT,
+	nm_usuario VARCHAR(45),
 	nm_email VARCHAR(45),
-	cd_senha VARCHAR(50)
+	cd_senha VARCHAR(50),
+    dt_nascimento DATE NOT NULL,
+    cd_cpf VARCHAR(11) NOT NULL,
+    cd_rg VARCHAR(9) NOT NULL,
+    dt_cadastro TIMESTAMP NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+    ON UPDATE CURRENT_TIMESTAMP
 );
-
-INSERT INTO tb_administrador VALUES (null,"Administrador Padrão","adm@adm","adm123");
