9d5d67a01e335bf5687822121d207befccf13997
Matheus2091 <matds.pires@gmail.com>
2019-10-30 20:21:03 -0300

    tb_estado e tb_cidade

--- script.sql
+++ script.sql
@@ -2,11 +2,18 @@ CREATE DATABASE db_evento;
 
 USE db_evento;
 
-CREATE TABLE tb_administrador(
-	cd_administrador INT PRIMARY KEY AUTO_INCREMENT,
-	nm_administrador VARCHAR(45),
-	nm_email VARCHAR(45),
-	cd_senha VARCHAR(50)
-);
+create table tb_estado(
+	cd_estado INT AUTO_INCREMENT,
+    nm_estado VARCHAR(45) NOT NULL,
+    PRIMARY KEY(cd_estado)
+    );
 
-INSERT INTO tb_administrador VALUES (null,"Administrador Padrão","adm@adm","adm123");
+create table tb_cidade(
+	cd_cidade INT AUTO_INCREMENT,
+    nm_cidade VARCHAR(45) NOT NULL,
+    id_estado INT NOT NULL,
+    PRIMARY KEY(cd_cidade),
+    CONSTRAINT CIDADE_ESTADO_FK
+    FOREIGN KEY(id_estado) REFERENCES tb_estado(cd_estado)
+    );
+    
