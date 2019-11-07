5639f4070d85a7ef88abea713b9c984c744aecdf
Matheus2091 <matds.pires@gmail.com>
2019-11-06 21:32:15 -0300

    Qualquer coisa

---
+++ bd/index.php
@@ -0,0 +1,42 @@
+<?php
+
+include 'header.php';
+
+?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-4">
+			
+
+		</div>
+		<div class="col-sm-4">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					Aqui vem o logotipo
+				</div>
+				<div class="panel-body">
+					<form class="form-group" method="post">
+						<label>Usuário:</label><br>
+						<input type="text" class="form-control" name="usuario" placeholder="Nome do usuário">
+						<br>
+						<label>Senha:</label><br>
+						<input type="password" class="form-control" name="senha" id="senha">
+						<br>
+						<button class="btn-success">Entrar</button>
+					</form>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<?php
+
+if(isset($_POST['email'], ))
+
+?>
+
+</body>
+</html>
