1b83679e9f38dfd4fe52aa2d27d5014f12583b0f
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2019-12-04 19:27:42 -0300

    Update

--- bd/bd/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = new mysqli("localhost", "root", "usbw", "db_evento");
-$con -> query ("SET NAMES 'utf8'");
-$con -> query ('SET character_set_connection = utf8');
-$con -> query ('SET character_set_client = utf8');
-$con -> query ('SET character_set_results = utf8');
-
-?>
--- bd/bd/session.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-$id = $_SESSION['id'];
-$usuario = $_SESSION['nome'];
-$tipo = $_SESSION['tipo'];
-
-?>
